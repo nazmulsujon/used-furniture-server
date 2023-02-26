cfe1eece1d4ee00205ec5ea3e37bc9ff0314415f
Nazmul Sujon <sujon686500@gmail.com>
2023-02-26 14:09:35 +0600

    server

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 const express = require("express");
 const cors = require("cors");
+// The dotenv package is a zero-dependency module that loads environment variables from a file into process.env. This allows developers to keep sensitive information like API keys, database credentials, and other configuration.
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 
 const port = process.env.PORT || 5000;
+
 //middle ware
 app.use(cors());
 app.use(express.json());
 
-const products = require("./data/products.json");
-
-app.get("/products", (req, res) => {
-  res.send(products);
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdzsrjb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-// app.get("/courses/:id", (req, res) => {
-//   const id = req.params.id;
-//   const selectedCourse = courses.find((course) => course.id === id);
-//   res.send(selectedCourse);
-// });
+async function run() {
+  try {
+    const productsCollection = client.db("UsedFurnitureDammam").collection("products");
 
-app.get("/", (req, res) => {
-  res.send("Used Furniture Server is running");
-});
+    // all products API
+    app.get("/products", async (req, res) => {
+      const query = {};
+
+      const products = await productsCollection.find(query).toArray();
+      res.send(products);
+    });
+
+    // get single product by id
+    app.get("/product/:id", async (req, res) => {
+      const requestId = req.params.id;
+      const query = { id: requestId };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.listen(port, () => {
   console.log("Used Furniture Server running", port);
